facdb9ca4b221a89d337d86684fa5e268e46492e
Minh-Long-NLU <91474004+Minh-Long-NLU@users.noreply.github.com>
2024-05-31 15:11:34 +0700

    commit

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -2,33 +2,39 @@ import "./featuredProperties.css";
 import useFetch from "../../hooks/useFetch";
 import React from 'react';
 
-
 const FeaturedProperties = () => {
-
   const { data, loading, error } = useFetch("/hotels?featured=true&limit=4");
 
   return (
     <div className="fp">
       {loading ? (
         "Loading, please wait!"
+      ) : error ? (
+        <span>Error: {error.message}</span>
       ) : (
         <>
-          {data.map((item) => (
-            <div className="fpItem" key={item._id}>
-              <img
-                src={item.photos[0]}
-                alt=""
-                className="fpImg"
-              />
-              <span className="fpName">{item.name}</span>
-              <span className="fpCity">{item.city}</span>
-              <span className="fpPrice">Starting from ${item.cheapestPrice}</span>
-              {item.rating && <div className="fpRating">
-                <button>{item.rating}</button>
-                <span>Excellent</span>
-              </div>}
-            </div>
-          ))}
+          {Array.isArray(data) ? (
+            data.map((item) => (
+              <div className="fpItem" key={item._id}>
+                <img
+                  src={item.photos[0]}
+                  alt=""
+                  className="fpImg"
+                />
+                <span className="fpName">{item.name}</span>
+                <span className="fpCity">{item.city}</span>
+                <span className="fpPrice">Starting from ${item.cheapestPrice}</span>
+                {item.rating && (
+                  <div className="fpRating">
+                    <button>{item.rating}</button>
+                    <span>Excellent</span>
+                  </div>
+                )}
+              </div>
+            ))
+          ) : (
+            <span>No featured properties found.</span>
+          )}
         </>
       )}
     </div>
